c48f57ae078ab8539e402966cadf8242847565c8
mmurtazaliev <mmurtazaliev@ozon.ru>
2021-10-08 14:12:53 +0300

    передача аргументов между фрагментами

--- app/src/main/java/com/mmurtazaliev/marsroverphotos/screens/HomeFragment.kt
+++ app/src/main/java/com/mmurtazaliev/marsroverphotos/screens/HomeFragment.kt
@@ -1,7 +1,20 @@
 package com.mmurtazaliev.marsroverphotos.screens
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import com.mmurtazaliev.marsroverphotos.R
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    companion object {
+        const val USER_NAME_KEY = "user_name"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<AppCompatTextView>(R.id.homeFragmentTitleTv).text = arguments
+            ?.getString(USER_NAME_KEY)
+    }
 }
--- app/src/main/java/com/mmurtazaliev/marsroverphotos/screens/MainFragment.kt
+++ app/src/main/java/com/mmurtazaliev/marsroverphotos/screens/MainFragment.kt
@@ -2,6 +2,8 @@ package com.mmurtazaliev.marsroverphotos.screens
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mmurtazaliev.marsroverphotos.R
@@ -9,9 +11,11 @@ import com.mmurtazaliev.marsroverphotos.R
 class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.postDelayed(
-            { findNavController().navigate(R.id.action_mainFragment_to_homeFragment) },
-            2500
-        )
+        view.findViewById<AppCompatTextView>(R.id.mainFragmentTitleTv).setOnClickListener {
+            findNavController().navigate(
+                R.id.action_mainFragment_to_homeFragment,
+                bundleOf(HomeFragment.USER_NAME_KEY to "Mikhail")
+            )
+        }
     }
 }
